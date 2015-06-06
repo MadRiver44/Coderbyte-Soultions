40b66c2552bd509a50b97a9ac2ba7596681f42d0
Kevin M. Turney <super996@optonline.net>
2015-06-06 12:48:11 -0400

    time convert solution using toFixed() method

---
+++ timeConvert.js
@@ -0,0 +1,20 @@
+/*Using the JavaScript language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours 
+and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). 
+Separate the number of hours and minutes with a colon. 
+*/
+
+
+function TimeConvert(num) { 
+
+    var hour = num / 60
+    var min = num % 60;
+    if (num < 60){
+        return 0 + ":"+ num;
+    }
+    return hour.toFixed() + ":" + min.toFixed();
+
+  // code goes here  
+  return num; 
+         
+}
+  
