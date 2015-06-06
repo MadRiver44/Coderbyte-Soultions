33b0e692edafd1d4d85117d2df193287c37e4ee0
Kevin M. Turney <super996@optonline.net>
2015-06-06 12:38:07 -0400

    factorial solution with full explanation of why this works

---
+++ firstFactorial.js
@@ -0,0 +1,23 @@
+function FirstFactorial(num){
+    var fact = num;
+    for (var i = num - 1; i > 1; i--){
+        fact *= i;
+        //console.log(num);
+        //console.log(i);
+        console.log(fact);
+    }
+    return fact;
+}
+FirstFactorial(8);
+
+/* Why this works
+
+The problem asks for the factorial (ex fact(4) = 4 * 3 * 2 * 1)
+First I had to initialize a variable  fact = num, to hold the result.
+with the for loop, I was looking to decrement to 1 and cycle down from
+num to 1. When the condition becomes false it would break the loop.
+
+First i starts at 3, the condition is true because it's greater than 1. Thus
+fact is incremented by multiplying by i. Fact is updated with the first value 4 * 3 (12).
+The loop starts again, num is now 3-1 (2), 2 is greater than 1, true. Fact, currently 12,
+is incremented by multiplying by 2. It kicks back up to decrement i.
