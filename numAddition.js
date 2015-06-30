fbb22be68ab35e0e33235a2da3ea753665d5c6cd
Kevin M. Turney <super996@optonline.net>
2015-06-29 20:07:20 -0400

    numAddition with comments, used parseInt and regex

---
+++ numAddition.js
@@ -0,0 +1,18 @@
+/*
+ Using the JavaScript language, have the function NumberSearch(str) take the str parameter, 
+ search for all the numbers in the string, add them together, then return that final number. 
+ For example: if str is "88Hello 3World!" the output should be 91. You will have to differentiate 
+ between single digit numbers and multiple digit numbers like in the example above. So "55Hello" 
+ and "5Hello 5" should return two different answers. Each string will contain at least one letter or symbol. 
+ */
+
+var str = "88Hello99 3World!"; // sample numberSring
+var sum = 0;
+
+var searchNum = function numberString(str){
+ var s = str.match(/\d+/g); // regex matched, tested via Regexer.com (great tool)
+     for (var i = 0; i < s.length; i++){ // loop through string
+         sum += parseInt(s[i]); // parseInt() parses a string and returns an integer of spec'd radix (base)
+        }return sum;
+     };
+searchNum(str);
