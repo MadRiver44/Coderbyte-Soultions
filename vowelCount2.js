b843c43e74983337bf23cb28d56615e63a2fb043
Kevin M. Turney <super996@optonline.net>
2016-09-30 12:03:26 -0400

    refactor with R egex

---
+++ vowelCount2.js
@@ -0,0 +1,49 @@
+/* 
+Have the function VowelCount(str) take the str string parameter being passed 
+and return the number of vowels the string contains (ie. "All cows eat grass" 
+would return 5). Do not count y as a vowel for this challenge. 
+*/
+
+//best with Regex
+
+function vowel(str){
+	return str.match(/a|e|i|o|u/gi).length;
+}
+
+vowel("all cows eat grass");
+
+
+// for loop
+
+function VowelCount(str) { 
+var vowels =  "aeiou";
+var count = 0;
+for (var i = 0; i < str.length; i++){
+    for (var q =  0; q < vowels.length; q++){
+       if(vowels[q] === str[i]){
+                count++;
+            }
+
+        }
+    }return count;
+}
+
+
+
+
+/* incorrect attempt
+
+function vowelCount(str) {
+	var count = 0;
+	var vowels = "aeiou";
+	str = str.toLowerCase();
+	for(var i = 0; i <str.length; i++){
+		if(vowels.indexOf(str[i])){
+			console.log(str[i]);
+			count++
+		}
+	}
+	console.log(count);
+}
+vowelCount("GOOd Job");
+*/
