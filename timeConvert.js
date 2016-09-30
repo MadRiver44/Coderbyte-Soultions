8ca9b52f45b6d8d09070aa391152c80eba5ae18b
Kevin M. Turney <super996@optonline.net>
2016-09-30 11:58:15 -0400

    refastor

--- timeConvert.js
+++ timeConvert.js
@@ -1,9 +1,21 @@
-/*Using the JavaScript language, have the function TimeConvert(num) take the num parameter being passed and return the number of hours 
+/*
+Using the JavaScript language, have the function TimeConvert(num) take the num parameter 
+being passed and return the number of hours 
 and minutes the parameter converts to (ie. if num = 63 then the output should be 1:3). 
 Separate the number of hours and minutes with a colon. 
 */
 
 
+// better
+function time(num){
+		var hours = Math.floor(num / 60);
+		var mins = num % 60; 
+		return hours + " : " + mins;
+}
+//time(63);
+
+
+// ok
 function TimeConvert(num) { 
 
     var hour = num / 60
