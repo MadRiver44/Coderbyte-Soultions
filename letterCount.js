2d4ad10faf1c87f48740c9e02f5d3e6ead17570c
Kevin M. Turney <super996@optonline.net>
2016-09-30 14:06:28 -0400

    modified

--- letterCount.js
+++ letterCount.js
@@ -1,5 +1,4 @@
 /*
-
 Have the function LetterCountI(str) take the str parameter being passed and return the first word
 with the greatest number of repeated letters. For example: "Today, is the greatest day ever!"
 should return greatest because it has 2 e's (and 2 t's) and it comes before ever which also has 2
@@ -38,4 +37,4 @@ function letterCount(str){
 	console.log(obj);
 	return answer["count"] === 1 ? -1 : answer["word"];
 }
-//letterCount("Today, is the greattest day ever!");
+letterCount("Today, is the greattest day ever!");
