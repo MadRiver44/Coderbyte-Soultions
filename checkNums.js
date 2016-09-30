7de7edc0b55a0033ad2dab6e80ff659f1bd006ac
Kevin M. Turney <super996@optonline.net>
2016-09-30 15:54:50 -0400

    refactor with ternary

--- checkNums.js
+++ checkNums.js
@@ -4,6 +4,13 @@
  If the parameter values are equal to each other then return the string -1. 
 */
 
+// better
+function CheckNums(num1, num2){
+	return (num1 === num2 ? -1 :(num1 > num2) ? true : false); 
+}
+CheckNums(34,39);
+
+
 function CheckNums(num1,num2) { 
     if (num2 > num1){
         return "true";
