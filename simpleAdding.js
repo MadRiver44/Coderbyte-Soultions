5b9f02ce7ff8c24513afa9fed0cd3ba225fe5b99
Kevin M. Turney <super996@optonline.net>
2016-09-30 11:44:09 -0400

    two versions

--- simpleAdding.js
+++ simpleAdding.js
@@ -1,14 +1,25 @@
 /*
 Using the JavaScript language, have the function SimpleAdding(num) 
-add up all the numbers from 1 to num. For the test cases, the parameter num will be any number from 1 to 1000. 
+add up all the numbers from 1 to num. For the test cases, the parameter 
+num will be any number from 1 to 1000. 
 */
 
+// for loop
+function simpleAdd(num){
+	var sum = 0;
+	for(var i = num; i > 0; i--){
+		sum += i;
+	}
+	return sum;
+}
+simpleAdd(1000);
 
+
+// while loop
 function SimpleAdding(num) { 
 var sum = 0;
-//function sumNum(num){
     while (num !==0){
-       sum +=num;
+       sum += num;
        num--;
-    }return sum; //return sum;
+    }return sum; 
 }
