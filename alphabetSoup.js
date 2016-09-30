2dc17462cc3476ed91f2479bcc3fd2692cffdde2
Kevin M. Turney <super996@optonline.net>
2016-09-30 10:14:00 -0400

    add version with sort compare function

--- alphabetSoup.js
+++ alphabetSoup.js
@@ -4,7 +4,17 @@ being passed and return the string with the letters in alphabetical order
 (ie. hello becomes ehllo). Assume numbers and punctuation symbols will not be included in the string. 
 */
 
+// preferred method with a sort comparison 
+function alpha(str){
+	return str.split("").sort(function(a,b){
+		return a > b;
+	}).join("");
+}
+
+// with no sort comparison
+
 function AlphabetSoup(str) { 
   return str.split("").sort().join("");
          
 }
+
