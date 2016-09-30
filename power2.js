92a7e274fc3a9f080858b4b341df6276e1a16559
Kevin M. Turney <super996@optonline.net>
2016-09-30 11:41:09 -0400

    refactor, with recursion

--- power2.js
+++ power2.js
@@ -1,7 +1,39 @@
- Using the JavaScript language, have the function PowersofTwo(num) take the num parameter being passed 
- which will be an integer and return the string true if it's a power of two. If it's not return the string 
- false. For example if the input is 16 then your program should return the string true but if the input 
- is 22 then the output should be the string false. 
+/*
+Have the function PowersofTwo(num) take the num parameter being passed which 
+will be an integer and return the string true if it's a power of two. If it's 
+not return the string false. For example if the input is 16 then your program 
+should return the string true but if the input is 22 then the output should be 
+the string false.
+*/
+
+
+//recursion
+
+function power2(num){
+	if(num / 2 === 1){
+		return true;
+	}else if(num % 2 !== 0){
+		return false;
+	}
+	console.log(num);
+	return power2(num/2);
+}
+power2(64);
+
+//or
+
+function power2(num){
+	while(num % 2 === 0 && num !== 0){
+		num = num / 2;
+			if(num === 2){
+			return true;
+		}
+		console.log(num);
+	}return false;
+}
+power2(32)
+
+// beginner...
 
 function power2(num){
     while (num % 2 === 0 && num !== 0){ 
